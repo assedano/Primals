4968d60f4225d37440c867feebdc358836322fbf
Anderson Sedano <andysda@outlook.com>
2024-09-07 18:05:13 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,57 @@
+let dados = [
+    
+    {
+        titulo : "Ifrit: A Encarnação do Fogo",
+        descricao : "A encarnação do fogo e da fúria, venerado pela tribo dos   amalj'aa, seres reptilianos. Mesmo entre os primais, Ifrit é especialmente selvagem, com um temperamento que explode em uma conflagração mortal. Seu sopro incendeia o próprio ar, suas garras derretem o aço mais resistente, e seus chifres retorcidos queimam os céus. Aqueles que ousam enfrentá-lo devem estar preparados para suportar as chamas do inferno.",
+        link: "https://finalfantasy.fandom.com/wiki/Ifrit_(Final_Fantasy_XIV)"
+    },
+
+    {
+        titulo: "Garuda: A Rainha da Tempestade",
+        descricao: " A rainha da tempestade, Garuda é a primal escolhida pelos Ixal. Embora sua forma seja elegante e seus movimentos graciosos, a natureza caprichosa e implacável dessa criatura sedenta de sangue gera terror irracional até mesmo entre seus devotos adoradores. Sua fúria frequentemente irrompe sem aviso, descendo sobre suas vítimas com uma fúria incontrolável.",
+        link: "https://finalfantasy.fandom.com/wiki/Garuda_(Final_Fantasy_XIV)"
+    },
+     
+    {
+        titulo: "Titan: O Gigante de Terra",
+        descricao: "A vontade indomável da terra personificada, amado pelos clãs kobold. Compassivo e gentil, Titã é como um pai para seus adoradores que habitam túneis. Porém, ai daqueles tolos o suficiente para provocar este colosso — pois, quando sua ira é despertada, a própria terra treme e se agita sob a fúria imparável de sua massa montanhosa.",
+        link: "https://finalfantasy.fandom.com/wiki/Titan_(Final_Fantasy_XIV)"
+    },
+    
+    {
+        titulo: "Leviathan: O Senhor dos Mares",
+        descricao: "Senhor de todas as águas, reverenciado pelos sahagin que habitam os oceanos. Nas profundezas sombrias, Leviatã devora tudo o que cruza seu caminho. Em eras passadas, os povos marítimos acreditavam que as ondas gigantes eram um sinal da ira da grande serpente, e buscavam apaziguá-lo com sacrifícios vivos.",
+        link: "https://finalfantasy.fandom.com/wiki/Leviathan_(Final_Fantasy_XIV)"
+    },
+
+    {
+        titulo: "Ramuh: O Ancião da Tempestade",
+        descricao: "O relâmpago em forma de sábio, venerado pelos sylphs. Embora descrito nas lendas como um ancião sábio e benevolente, guardião de segredos antigos, Ramuh é um árbitro implacável contra aqueles que profanam as profundezas sagradas da floresta, fulminando invasores com poderosos raios de trovão.",
+        link: "https://finalfantasy.fandom.com/wiki/Ramuh_(Final_Fantasy_XIV)"
+    },
+
+    {
+        titulo: "Shiva: A Senhora do Gelo",
+        descricao: "Shiva é um dos Primals mais icônicos de Final Fantasy XIV, representando a beleza e a brutalidade do inverno. A protetora sagrada e primal dos Harriers, ela é frequentemente descrita como uma guerreira elegante e implacável, capaz de congelar até mesmo os corações mais fortes.",
+        link: "https://finalfantasy.fandom.com/wiki/Shiva_(Final_Fantasy_XIV)"
+    },
+
+    {
+        titulo: "Odin",
+        descricao: "Um cavaleiro sombrio vestido inteiramente de preto, Odin vaga por caminhos há muito esquecidos sob a copa da Floresta do Leste. Não há ninguém vivo que conheça a verdade sobre quando ou por quem esse antigo primal foi invocado pela primeira vez no mundo. Sempre vigilante enquanto cavalga sua poderosa montaria, Sleipnir, diz-se que Odin é movido unicamente pelo desejo de travar batalhas contra oponentes dignos.",
+        link: "https://finalfantasy.fandom.com/wiki/Odin_(Final_Fantasy_XIV)"
+    },
+
+    {
+        titulo: "Good King Moggle Mog XII",
+        descricao: "Good King Moggle Mog XII é uma figura lendária em Final Fantasy XIV, especialmente para os Moogles. Ele é retratado como um governante justo e benevolente. Um Primal peculiar, apesar de sua aparência amigável, ele pode ser uma força a ser considerada.",
+        link: "https://finalfantasy.fandom.com/wiki/Good_King_Moggle_Mog_XII"
+    },
+
+    {
+        titulo: "Ultima Weapon",
+        descricao: "A Ultima Weapon é a personificação do poder destrutivo, uma arma lendária capaz de causar caos e devastação em escala global.",
+        link: "https://finalfantasy.fandom.com/wiki/Ultima_Weapon_(Final_Fantasy_XIV)"
+    },
+
+    ];
